3d88615ac11ef4d66362bacd7c2543e3907e9c40
kartikmadan26 <kartikmadan26@gmail.com>
2025-06-01 20:39:23 +0530

    initial setup

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,30 @@
+package com.pm.billingservice.grpc;
+
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import billing.BillingResponse;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver) {
+
+        log.info("Create billing account request received {}", billingRequest.toString());
+
+        // Business logic
+
+
+        BillingResponse response=BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("Active")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,70 @@
+package com.pm.patientservice.service;
+
+import com.pm.patientservice.dto.PatientRequestDto;
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.exception.EmailAlreadyExistsException;
+import com.pm.patientservice.exception.PatientNotFoundException;
+import com.pm.patientservice.grpc.BillingServiceGrpcClient;
+import com.pm.patientservice.kafka.KafkaProducer;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class PatientService {
+    private final PatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
+    private final KafkaProducer kafkaProducer;
+
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient, KafkaProducer kafkaProducer) {
+        this.patientRepository = patientRepository;
+        this.billingServiceGrpcClient = billingServiceGrpcClient;
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+        List<PatientResponseDTO> patientResponseDTOs = new ArrayList<>();
+        for (Patient patient : patients) {
+            patientResponseDTOs.add(PatientMapper.toDTO(patient));
+        }
+        return patientResponseDTOs;
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDto patientRequestDto){
+        if(patientRepository.existsByEmail(patientRequestDto.getEmail())){
+            throw new EmailAlreadyExistsException("A patient with this email already exists" + patientRequestDto.getEmail());
+        }
+        Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDto));
+        billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),newPatient.getName(),newPatient.getEmail());
+        kafkaProducer.sendEvent(newPatient);
+        return PatientMapper.toDTO(newPatient);
+    }
+
+    public PatientResponseDTO updatePatient(UUID id, PatientRequestDto patientRequestDto){
+        Patient patient = patientRepository.findById(id).orElseThrow(() -> new PatientNotFoundException("Patient not found with ID: "+id));
+
+        if(patientRepository.existsByEmailAndIdNot(patientRequestDto.getEmail(),id)){
+            throw new EmailAlreadyExistsException("A patient with this email already exists" + patientRequestDto.getEmail());
+        }
+
+        patient.setName(patientRequestDto.getName());
+        patient.setEmail(patientRequestDto.getEmail());
+        patient.setAddress(patientRequestDto.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDto.getDateOfBirth()));
+
+        Patient updatedPatient = patientRepository.save(patient);
+        return PatientMapper.toDTO(updatedPatient);
+
+    }
+
+    public void deletePatient(UUID id){
+        patientRepository.deleteById(id);
+    }
+}
